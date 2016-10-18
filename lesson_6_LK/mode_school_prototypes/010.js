a771023ae5afdea4d6a34d76331dd40ab3adf0ec
lkitto <lkitto@gsu.edu>
2016-10-18 13:24:10 -0400

    node school

---
+++ lesson_6_LK/mode_school_prototypes/010.js
@@ -0,0 +1,10 @@
+var robot = {
+  smart: true
+}
+claim(robot.smart, true);
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+	robot: robot
+}
