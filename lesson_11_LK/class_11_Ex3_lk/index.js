b1b47823d83fc6c5b643105eadde58c3ffcc9c9a
lkitto <lkitto@gsu.edu>
2016-11-06 08:56:58 -0500

    cleaning cache

--- lesson_11_LK/class_11_Ex3_lk/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lkittogsuedu on 11/1/16.
- */
-var Data = require('./restData');
-var Timestamp = require('./timeStamp');
-
-
-//Stuff into set interval
-Data.getData(1).then(Timestamp.getTimes());
